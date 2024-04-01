e44eff6993a08301a6df80ac8c66ce2bdd33333b
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-04-01 11:48:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
           <Route path='/about' element={<About/>}/> 
           <Route path='/services' element={<Services/>}/> 
           <Route path='/contact' element={<Contact/>}/> 
-          <Route path='*' element={<Lost/>}></Route>
+          <Route path='https://form-routes-project.vercel.app/*' element={<Lost/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
